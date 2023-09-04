0f3b8f0a5d5efbb43916069a1524e69e932ce927
Prachi24599 <prachi24599@gmail.com>
2023-09-04 23:48:22 +0530

    structure created

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Counter = () => {
-  return <div></div>;
+  return (
+    <div>
+      <button>increment</button>
+      <br />
+      <br />
+      <div>{}</div>
+      <br />
+      <br />
+      <button>Decrement</button>
+    </div>
+  );
 };
 
 export default Counter;
