71734baae2860fe3443cab9e68744802e44a9ad6
Prachi24599 <prachi24599@gmail.com>
2023-09-05 13:17:14 +0530

    useDispatch hook to fetch the functions

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "../redux/slices/CounterSlice";
 
 const Counter = () => {
   const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <div>
-      <button>increment</button>
+      <button onClick={() => dispatch(increment())}>increment</button>
       <br />
       <br />
       <div>{count}</div>
       <br />
-      <br />
-      <button>Decrement</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
     </div>
   );
 };
